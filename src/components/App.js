7e35d84ce0b7bb29e4bf3dc841bc94bb6db9856f
unknown <ddidce@gmail.com>
2020-10-28 02:01:52 +0900

    페이지 로딩 실패

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 /** @format */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/SignUp.css';
+import Login from '../routes/Login';
+import Loading from './Loading';
 import AppRouter from './Router';
 
 const App = () => {
+    // const [isLoading, setIsLoading] = useState(true);
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setIsLoading(false);
+    //         console.log('하이');
+    //     }, 2000);
+    //     console.log('끝');
+    //     return () => clearTimeout(isLoading);
+    // }, []);
+
     return <AppRouter />;
+    // <>{isLoading ? <Loading /> : <Login />}</>;
 };
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,30 +1,12 @@
 /** @format */
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '../routes/Login';
 import SignUp from '../routes/SignUp';
 import Loading from './Loading';
 
 const AppRouter = () => {
-    // const [isLoading, setIsLoading] = useState(true);
-    // const Loading = () => (
-    //     <div>
-    //         <Loading />
-    //     </div>
-    // );
-    // const Login = () => (
-    //     <div>
-    //         <Login />
-    //     </div>
-    // );
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setIsLoading(false);
-    //     }, 3000);
-    // }, []);
-
     return (
         <Router>
             <Switch>
